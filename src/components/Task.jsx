34e3bdd184dab6d379ccfd6e914b857b885a0ee1
Dhananjay Bhatia <dhananjaybhatia@yahoo.co.in>
2024-10-06 18:29:54 +1100

    new audio update

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,11 @@ import { FaTimes } from "react-icons/fa";
 
 export default function Task({ task, onDelete }) {
   // Path to your audio file (update the path accordingly)
-  const alarmSound = new Audio("/sound/sound.wav");
+  // const alarmSound = new Audio("/sound/sound.wav");
+  const alarmSound = new Audio(
+    "https://dhananjaybhatia.github.io/TaskTracker/sound/sound.wav"
+  );
+
   // const alarmSound = new Audio(`${process.env.PUBLIC_URL}/sound/sound.wav`);
 
   // Function to trigger an alarm based on task's reminder time
